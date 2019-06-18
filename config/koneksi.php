96e7179a0a69e8287b8d895c4e4b237b2c0e2a55
Reyhan Firnas <firnasreyhan@gmail.com>
2019-06-18 19:59:47 +0700

    First Commit

---
+++ config/koneksi.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Menggunakan mysqli_connect untuk menghubungkan php dengan mysql
+ 
+
+$databaseHost = 'localhost';
+$databaseName = 'dbsiswa';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+// mengecek koneksi
+if (!$mysqli) {
+    //die("Koneksi gagal: " . mysqli_connect_error());
+}else {
+    //echo "Koneksi berhasil";
+}
+*/
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Memanggil css bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-3">
+			<h1>Hai, ini index :)</h1>
+		</div>
+		<div class="col-md-3"></div>
+	</div>
+
+    <!-- Memanggil jQuery -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+
+    <!-- Memanggil Bootstrap js -->
+    <script src="bootstrap.min.js"></script>
+
+</body>
+</html>
